46c583bc07b882788dd879bb1ea2fb267dd35ef6
Pranay-Web0Dev <dev.pranay.kadav@gmail.com>
2025-09-25 19:44:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ io.on("connection", (socket) => {
 
     })
 })
+const PORT = process.env.PORT || 3000
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("server is running on port 3000")
 })
